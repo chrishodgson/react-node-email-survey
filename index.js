005eac904ee007ab6fb4f8af3b73f6cadc4ac8e7
Chris Hodgson <hello@chrishodgsonweb.co.uk>
2018-10-04 08:49:51 +0100

    heroku build

--- index.js
+++ index.js
@@ -3,5 +3,5 @@ const app = express();
 app.get('/', (req, res) => {
   res.send({hi: 'there'});
 });
-const PORT  = process.env.port || 5000;
+const PORT  = process.env.PORT || 5000;
 app.listen(PORT);
