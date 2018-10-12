1e113290a5d051edc9669dc35473b492fe9b19b4
Chris Hodgson <hello@chrishodgsonweb.co.uk>
2018-10-12 08:56:43 +0100

    start reducers

--- client/src/index.js
+++ client/src/index.js
@@ -1,4 +1,16 @@
-import React from react;
-import ReactDom from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware } from "redux";
 
-ReactDom.render();
+import App from "./components/App";
+import reducers from './reducers';
+
+const store = createStore(reducers, {}, applyMiddleware());
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.querySelector("#root")
+);
