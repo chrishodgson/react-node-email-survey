1a1bdca3db6891e06a8f38bbb5f06958f813edb0
Chris Hodgson <chris.3.hodgson@plus.net>
2018-10-19 15:53:52 +0100

    redux form

---
+++ client/src/components/surveys/SurveyForm.js
@@ -0,0 +1,47 @@
+import _ from "lodash";
+import React, { Component } from "react";
+import { reduxForm, Field } from "redux-form";
+import SurveyField from "./SurveyField";
+import { Link } from "react-router-dom";
+
+const FIELDS = [
+  { label: "Survey Title", name: "title" },
+  { label: "Survey Line", name: "subject" },
+  { label: "Email Body", name: "body" },
+  { label: "Recipient List", name: "emails" }
+];
+
+class SurveyForm extends Component {
+  renderFields() {
+    return _.map(FIELDS, ({ label, name }) => {
+      return (
+        <Field
+          key={name}
+          type="text"
+          component={SurveyField}
+          label={label}
+          name={name}
+        />
+      );
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <form onSubmit={this.props.handleSubmit(values => console.log(values))}>
+          {this.renderFields()}
+          <Link to="/surveys" className="red btn-flat white-text">Cancel</Link>
+          <button type="submit" className="teal btn-flat white-text right">
+            Next
+            <i className="material-icons right">done</i>
+          </button>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default reduxForm({
+  form: "surveyForm"
+})(SurveyForm);
